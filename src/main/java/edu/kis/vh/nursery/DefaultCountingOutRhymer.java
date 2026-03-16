67e1a802143508f36be32375aa9f31fafa5fe0e5
Zic-gamedev <kacper.sobierajxd@gmail.com>
2026-03-16 11:43:01 +0100

    6. Usunięcie magic numers z pliku DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int MAX = 12;
+    public static final int ERROR_CODE = -1;
+    public static final int ELEVEN = 11;
+    private int[] numbers = new int[MAX];
 
-    public int total = -1;
+    public int total = ERROR_CODE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == ERROR_CODE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == ELEVEN;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERROR_CODE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERROR_CODE;
         return numbers[total--];
     }
 
